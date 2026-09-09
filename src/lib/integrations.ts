42768187c9b22402f45139c571249085c2311d60
alexsulpizio-stack <alex.sulpizio@gmail.com>
2026-09-09 13:08:30 -0400

    Switch GA4 to working data stream

--- src/lib/integrations.ts
+++ src/lib/integrations.ts
@@ -1,5 +1,5 @@
 export const GSC_VERIFICATION = "";
-export const GA4_MEASUREMENT_ID = "G-YCF479SQGQ";
+export const GA4_MEASUREMENT_ID = "G-WFQ4KMBMRP";
 export const CLOUDFLARE_ANALYTICS_TOKEN = "";
 export const CONSENT_STORAGE_KEY = "pixeltools-consent-v1";
 export type ConsentChoice = "accepted" | "declined";
